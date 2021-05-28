e2cec2bd86d5a31d8c8f0afee1b21c17d864689c
AnArzhanykh <45142693+AnArzhanykh@users.noreply.github.com>
2021-05-28 18:40:22 +0300

    1`

--- src/js/renderCountry.js
+++ src/js/renderCountry.js
@@ -1,15 +1,23 @@
+import ListCountries from '../template/ListCountries'
+import SearchCountry from '../template/searchCountry'
 
 export default function renderCountry(country){
-
-    const arr = country.then(arr => arr);
-    console.log(arr);
-    if(arr.length > 10){
-        console.log(arr.length);
-    } 
-    if(arr.length <= 10){
-        
-    }
-    if(arr.length === 1){
-        
-    }
-}
+    const countryContainerRef = document.querySelector('.country')
+    country.then(arr =>{
+        console.log(arr);
+        countryContainerRef.removeChild()
+        if(arr.length > 10){
+            
+        } 
+        if( 1 < arr.length && arr.length <= 10) {
+            const markup = ListCountries(arr)
+            countryContainerRef.insertAdjacentHTML('beforeend', markup);
+        }
+        if(arr.length === 1){
+            const markup = SearchCountry(arr)
+            countryContainerRef.insertAdjacentHTML('beforeend', markup);
+            console.log(markup);
+        }
+    });
+    
+}  
