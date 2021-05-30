346b3cdd50a5b999e73bc98656eb1fd3de032a25
AnArzhanykh <45142693+AnArzhanykh@users.noreply.github.com>
2021-05-30 17:08:17 +0300

    finaly

--- src/js/renderCountry.js
+++ src/js/renderCountry.js
@@ -2,14 +2,17 @@ import ListCountries from '../template/ListCountries'
 import SearchCountry from '../template/searchCountry'
 import refInputSearch from '../index.js'
 
-import { alert, defaultModules } from '@pnotify/core';
-import * as PNotifyMobile from '@pnotify/mobile';
 
-defaultModules.set(PNotifyMobile, {});
+
+import { alert, notice, info, success, error, defaultModules } from'@pnotify/core';
+import"@pnotify/core/dist/PNotify.css";
+import"@pnotify/core/dist/BrightTheme.css";
+
 
 
 
 export default function renderCountry(country){
+
     const countryContainerRef = document.querySelector('.country');
 
     while (countryContainerRef.firstChild) {
@@ -19,8 +22,8 @@ export default function renderCountry(country){
     country.then(arr =>{
         console.log(arr);
         if(arr.length > 10){
-                    alert({
-            text: 'Notice me, senpai!'
+            const myError = error({
+                text:'Too many matches found. Please enter a more specific quary!'
             });
         } 
         if( 1 < arr.length && arr.length <= 10) {
